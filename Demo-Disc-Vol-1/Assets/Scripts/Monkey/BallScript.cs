69e6db0db44cbab78002d6706a90ec90358cf823
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2024-12-25 03:35:10 -0800

    Mnky: Created base for player.
    Ball, Ball material, test stage, camera follow, speed control.

---
+++ Demo-Disc-Vol-1/Assets/Scripts/Monkey/BallScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    [SerializeField] float maxVelocity;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float acceleration;
+    [SerializeField] GameObject parentContainer;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = maxVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.velocity.z < maxSpeed)
+        {
+            rb.AddForce(parentContainer.transform.forward * acceleration, ForceMode.Acceleration);
+        }
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log(rb.velocity.z);
+    }
+}

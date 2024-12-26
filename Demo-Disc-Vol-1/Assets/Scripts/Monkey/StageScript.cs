b16558e4765316c11b07def20928208af432a540
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2024-12-26 04:43:56 -0800

    Mnky: first pass of player position based tilt

--- Demo-Disc-Vol-1/Assets/Scripts/Monkey/StageScript.cs
+++ Demo-Disc-Vol-1/Assets/Scripts/Monkey/StageScript.cs
@@ -6,6 +6,8 @@ public class StageScript : MonoBehaviour
 {
     Rigidbody rb;
 
+    [SerializeField] GameObject playerBall;
+    [SerializeField] float rotateSpeed;
     [SerializeField] float horizontalSpeed;
     [SerializeField] float verticalSpeed;
     [SerializeField] float returnSpeed;
@@ -25,9 +27,15 @@ public class StageScript : MonoBehaviour
         }
         else
         {
-            
-            Quaternion deltaRotation = Quaternion.Euler(Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime, 0, -Input.GetAxis("Horizontal") *     horizontalSpeed * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotation);
+            transform.RotateAround(playerBall.transform.position, Vector3.back, Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime);
+            transform.RotateAround(playerBall.transform.position, Vector3.right, Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime);
+
+
+            //transform.RotateAround(playerBall.transform.position, new Vector3(Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime, 0 , //Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime), rotateSpeed);
+
+
+           //Quaternion deltaRotation = Quaternion.Euler(Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime, 0, -Input.GetAxis("Horizontal") /*   /  horizontalSpeed * Time.deltaTime);
+           //rb.MoveRotation(rb.rotation * deltaRotation);
         }
     }
 }

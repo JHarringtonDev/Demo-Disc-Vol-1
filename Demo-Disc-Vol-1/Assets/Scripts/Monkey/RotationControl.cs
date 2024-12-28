7262a56ec18bcf4933fdc8b22f1ab856a193a477
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2024-12-28 04:42:08 -0800

    Mnky: Adjustments to stage control and feel

--- Demo-Disc-Vol-1/Assets/Scripts/Monkey/RotationControl.cs
+++ Demo-Disc-Vol-1/Assets/Scripts/Monkey/RotationControl.cs
@@ -18,48 +18,45 @@ public class RotationControl : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
+        transform.position = Vector3.zero;
+
+        if(Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
         {
-            transform.position = Vector3.zero;
             transform.rotation = (Quaternion.RotateTowards(transform.rotation, Quaternion.identity, returnSpeed * Time.deltaTime));
         }
-        else
+        
+        float vertRotation = Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
+        float horiRotation = -Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime;
+        
+        if(vertRotation > 0)
         {
-
-            float vertRotation = Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
-            float horiRotation = -Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime;
-            Debug.Log(vertRotation);
-            
-
-            if(vertRotation > 0)
+            if(Mathf.Floor(transform.localEulerAngles.x) <= 20 || Mathf.Floor(transform.localEulerAngles.x) >= 330)
             {
-                if(Mathf.Floor(transform.localEulerAngles.x) <= 20 || Mathf.Floor(transform.localEulerAngles.x) >= 330)
-                {
-                    transform.RotateAround(Vector3.zero, playerCamera.transform.right, vertRotation);
-                }
-            }
-            else if(vertRotation < 0)
-            {
-                if(Mathf.Floor(transform.localEulerAngles.x) >= 340 || Mathf.Floor(transform.localEulerAngles.x) <= 30)
-                {
-                    transform.RotateAround(Vector3.zero, playerCamera.transform.right, vertRotation);
-                }
-            }
-
-            if (horiRotation > 0)
-            {
-                if(Mathf.Floor(transform.localEulerAngles.z) <= 20 || Mathf.Floor(transform.localEulerAngles.z) >= 330)
-                {
-                    transform.RotateAround(Vector3.zero, playerCamera.transform.forward, horiRotation);
-                }
-            }
-            else if (horiRotation < 0)
-            {
-                if (Mathf.Floor(transform.localEulerAngles.z) >= 340 || Mathf.Floor(transform.localEulerAngles.z) <= 30)
-                {
-                    transform.RotateAround(Vector3.zero, playerCamera.transform.forward, horiRotation);
-                }
+                transform.RotateAround(Vector3.zero, playerCamera.transform.right, vertRotation);
             }
         }
+        else if(vertRotation < 0)
+        {
+            if(Mathf.Floor(transform.localEulerAngles.x) >= 340 || Mathf.Floor(transform.localEulerAngles.x) <= 30)
+            {
+                transform.RotateAround(Vector3.zero, playerCamera.transform.right, vertRotation);
+            }
+        }
+        
+        if (horiRotation > 0)
+        {
+            if(Mathf.Floor(transform.localEulerAngles.z) <= 20 || Mathf.Floor(transform.localEulerAngles.z) >= 330)
+            {
+                transform.RotateAround(Vector3.zero, playerCamera.transform.forward, horiRotation);
+            }
+        }
+        else if (horiRotation < 0)
+        {
+            if (Mathf.Floor(transform.localEulerAngles.z) >= 340 || Mathf.Floor(transform.localEulerAngles.z) <= 30)
+            {
+                transform.RotateAround(Vector3.zero, playerCamera.transform.forward, horiRotation);
+            }
+        }
+        }
     }
-}
+

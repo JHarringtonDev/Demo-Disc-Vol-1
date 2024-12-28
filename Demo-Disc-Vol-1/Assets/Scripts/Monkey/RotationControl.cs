fe019e630308937bea9a167adc8deafe1a538489
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2024-12-28 03:23:08 -0800

    Mnky: added limits to stage rotation angle

--- Demo-Disc-Vol-1/Assets/Scripts/Monkey/RotationControl.cs
+++ Demo-Disc-Vol-1/Assets/Scripts/Monkey/RotationControl.cs
@@ -16,7 +16,7 @@ public class RotationControl : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0)
         {
@@ -25,11 +25,41 @@ public class RotationControl : MonoBehaviour
         }
         else
         {
+
             float vertRotation = Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
             float horiRotation = -Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime;
+            Debug.Log(vertRotation);
+            
 
-            transform.RotateAround(Vector3.zero, playerCamera.transform.right, vertRotation);
-            transform.RotateAround(Vector3.zero, playerCamera.transform.forward, horiRotation);
+            if(vertRotation > 0)
+            {
+                if(Mathf.Floor(transform.localEulerAngles.x) <= 20 || Mathf.Floor(transform.localEulerAngles.x) >= 330)
+                {
+                    transform.RotateAround(Vector3.zero, playerCamera.transform.right, vertRotation);
+                }
+            }
+            else if(vertRotation < 0)
+            {
+                if(Mathf.Floor(transform.localEulerAngles.x) >= 340 || Mathf.Floor(transform.localEulerAngles.x) <= 30)
+                {
+                    transform.RotateAround(Vector3.zero, playerCamera.transform.right, vertRotation);
+                }
+            }
+
+            if (horiRotation > 0)
+            {
+                if(Mathf.Floor(transform.localEulerAngles.z) <= 20 || Mathf.Floor(transform.localEulerAngles.z) >= 330)
+                {
+                    transform.RotateAround(Vector3.zero, playerCamera.transform.forward, horiRotation);
+                }
+            }
+            else if (horiRotation < 0)
+            {
+                if (Mathf.Floor(transform.localEulerAngles.z) >= 340 || Mathf.Floor(transform.localEulerAngles.z) <= 30)
+                {
+                    transform.RotateAround(Vector3.zero, playerCamera.transform.forward, horiRotation);
+                }
+            }
         }
     }
 }

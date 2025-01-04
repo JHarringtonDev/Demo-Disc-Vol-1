d8ed691b6c76378823fa6fc26768e6b72a1a9773
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2025-01-04 01:56:23 -0800

    Ants: added base for games click controls

--- Demo-Disc-Vol-1/Assets/Scripts/Ants/ClickDetection.cs
+++ Demo-Disc-Vol-1/Assets/Scripts/Ants/ClickDetection.cs
@@ -16,7 +16,29 @@ public class ClickDetection : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 100.0f, clickMask))
             {
                 clickPosition = hit.point;
-                Debug.Log(clickPosition);
+                Debug.Log("move player to " + clickPosition);
+            }
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 clickPosition = -Vector3.one;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f, clickMask))
+            {
+                clickPosition = hit.point;
+                Debug.Log("send ant to " + clickPosition);
+            }
+        }
+        else if (Input.GetMouseButtonDown(2))
+        {
+            Vector3 clickPosition = -Vector3.one;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f, clickMask))
+            {
+                clickPosition = hit.point;
+                Debug.Log("ants in range of " + clickPosition + " return to player");
             }
         }
     }

0ba973aa37fd24b9ea0bd5d87d6766595389bfa5
Jason Harrington <61073321+JHarringtonDev@users.noreply.github.com>
2025-01-24 02:16:23 -0800

    Fant: Health bar changes color based on HP

--- Demo-Disc-Vol-1/Assets/Scripts/Fantasy/BattleScript.cs
+++ Demo-Disc-Vol-1/Assets/Scripts/Fantasy/BattleScript.cs
@@ -54,7 +54,7 @@ public class BattleScript : MonoBehaviour
 
     public void Item()
     {
-        stats.DamagePlayer(5);
+        stats.DamagePlayer(2);
     }
 
     public void Run() 
@@ -75,5 +75,18 @@ public class BattleScript : MonoBehaviour
         MPNumber.text = $"{currentMP}";
         HPBar.fillAmount =  currentHP / maxHP ; 
         MPBar.fillAmount = currentMP / maxMP; 
+
+        if(currentHP <= maxHP / 3)
+        {
+            HPBar.color = Color.red;
+        }
+        else if(currentHP <= maxHP / 2)
+        {
+            HPBar.color = Color.yellow;
+        }
+        else 
+        { 
+            HPBar.color = Color.green;
+        }
     }
 }
